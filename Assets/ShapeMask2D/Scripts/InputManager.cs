1097af63cbf7e45013ba1885899f7609eb60b0de
katelevshova <11220246+katelevshova@users.noreply.github.com>
2020-10-02 17:53:58 -0700

    Task1: implemented double click in the InputManager

--- Assets/ShapeMask2D/Scripts/InputManager.cs
+++ Assets/ShapeMask2D/Scripts/InputManager.cs
@@ -1,12 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class InputManager : MonoBehaviour
+public class InputManager : MonoBehaviour, IPointerClickHandler
 {
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerClick");
+    }
+
+    /**
+* #if UNITY_ANDROID || UNITY_IOS || UNITY_WP8
+           //...and if it isn't using touch aiming, leave
+           //...and if it isn't using touch aiming, leave
+           if (!isTouchAiming)
+               return;
+#else
+   //...otherwise, record the mouse's position to the screenPosition variable
+   screenPosition = Input.mousePosition;
+#endif*/
+
+
     // Update is called once per frame
     void Update()
     {
+
+
         if (Input.GetMouseButtonDown(0))
         {
             Ray rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -23,4 +43,6 @@ public class InputManager : MonoBehaviour
             }
         }
     }
+
+    
 }
