e817965737e98f30ceb5d654686b17ab02862fe8
katelevshova <11220246+katelevshova@users.noreply.github.com>
2020-10-03 11:27:46 -0700

    Added MenuBtns gameObject, changed the Canvas order for MenuBtns, moved Sprites folder into Resources

--- Assets/ShapeMask2D/Scripts/GameController.cs
+++ Assets/ShapeMask2D/Scripts/GameController.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     public static GameController Instance;
+    public Button btnChangeSpriteMask;
+    public ShapeSpriteMask shapeSpriteMask;
 
     public void Awake()
     {
@@ -17,10 +21,21 @@ public class GameController : MonoBehaviour
         {
             Destroy(this);
         }
+
+        if (btnChangeSpriteMask == null || shapeSpriteMask == null)
+        {
+            throw new Exception("Initialize GameController properties in the Editor, drag from Hierarchy window");
+        }
     }
 
     void Start()
     {
         Debug.Log("[GameController] start");
+    }
+
+    public void OnBtnChangeSpriteMaskClickHandler()
+    {
+        Debug.Log("->OnBtnChangeSpriteMaskClickHandler");
+        shapeSpriteMask.UpdateShapeToNext();
     }
 }
---
+++ Assets/ShapeMask2D/Scripts/ShapeSpriteMask.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeSpriteMask : MonoBehaviour
+{
+    private SpriteMask _spriteMask; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _spriteMask = GetComponentInChildren<SpriteMask>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateShapeToNext()
+    {
+        string shape_sprite_path = "Sprites/" + "Heart";
+        _spriteMask.sprite = Resources.Load<Sprite>(shape_sprite_path);
+    }
+}
