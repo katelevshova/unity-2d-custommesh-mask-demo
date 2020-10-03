f0ad80823560a488cb7cc526ce0451dbacf9fb04
katelevshova <11220246+katelevshova@users.noreply.github.com>
2020-10-02 18:04:15 -0700

    Task1: implemented double click, added TransparentImage inside Input Manager

--- Assets/ShapeMask2D/Scripts/InputManager.cs
+++ Assets/ShapeMask2D/Scripts/InputManager.cs
@@ -2,47 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Button))]   // for working  OnPointerClick
 public class InputManager : MonoBehaviour, IPointerClickHandler
 {
+    public static InputManager Instance;
+    [SerializeField]
+    private float timeClickDelay = 1f;
+    private float _lastTimeClick;
+
     public void OnPointerClick(PointerEventData eventData)
     {
-        Debug.Log("OnPointerClick");
-    }
-
-    /**
-* #if UNITY_ANDROID || UNITY_IOS || UNITY_WP8
-           //...and if it isn't using touch aiming, leave
-           //...and if it isn't using touch aiming, leave
-           if (!isTouchAiming)
-               return;
-#else
-   //...otherwise, record the mouse's position to the screenPosition variable
-   screenPosition = Input.mousePosition;
-#endif*/
-
-
-    // Update is called once per frame
-    void Update()
-    {
-
-
-        if (Input.GetMouseButtonDown(0))
+        // MOBILE
+        if (eventData.clickCount == 1)
         {
-            Ray rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hitInfo;
-            if (Physics.Raycast(rayOrigin, out hitInfo))
-            {
-                //TODO: here we can use Interface if we will have lots of different shapes in future
-                HexagonRenderer hexagon = hitInfo.collider.GetComponent<HexagonRenderer>();
+            float currentTimeClick = eventData.clickTime;
 
-                if (hexagon != null)
-                {
-                    Debug.Log("InputManager, clicked!");
-                }
+            if (Mathf.Abs(currentTimeClick - _lastTimeClick) < timeClickDelay)
+            {
+                //Debug.Log("[InputManager]->OnPointerClick:MOBILE double click");
+                DoubleClickHandler();
             }
+            _lastTimeClick = currentTimeClick;
+        }
+
+        // PC
+        if (eventData.clickCount == 2)
+        {
+            //Debug.Log("[InputManager]->OnPointerClick: PC double click");
+            DoubleClickHandler();
         }
     }
 
-    
+    private void DoubleClickHandler()
+    {
+        //Debug.Log("[InputManager]->DoubleClickHandler");
+        
+        Ray rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(rayOrigin, out hitInfo))
+        {
+            //TODO: here we can use Interface if we will have lots of different shapes in future
+            HexagonRenderer hexagon = hitInfo.collider.GetComponent<HexagonRenderer>();
+
+            if (hexagon != null)
+            {
+                Debug.Log("[InputManager]->DoubleClickHandler: Hexagon!");
+            }
+        }
+    }
 }
