b81f5296eeb7df518c8a523777c65b215a36628d
katelevshova <11220246+katelevshova@users.noreply.github.com>
2020-10-03 10:54:29 -0700

    Changes in ImputManager and HexagonRenderer (not everything is working yet)

--- Assets/ShapeMask2D/Scripts/HexagonRenderer.cs
+++ Assets/ShapeMask2D/Scripts/HexagonRenderer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,33 +14,130 @@ public class HexagonRenderer : MonoBehaviour
     private Material _material;
     private MeshCollider _meshCollider;
 
+    private Vector3 _screenPoint;
+    private Vector3 _offset;
+
+    [HideInInspector]
+    Vector3[] vertices = new Vector3[6];
+
 
     // Start is called before the first frame update
     void Start()
     {
         _meshCollider = GetComponent<MeshCollider>();
+        InputManager.OnDoubleClickHandler += OnDoubleClickHandler;
         DrawHexagon();
     }
 
-   
+    void Update()
+    {
+        /*if (InputManager.Instance == null || !InputManager.Instance.isValid)
+            return;
+
+        Vector3 distance = _offset.normalized;
+        float dotProd = Vector3.Dot(transform.right, distance);
+
+        if (Mathf.Abs(1 - dotProd) < 1f)
+        {
+            Debug.Log("[HexagonRenderer]-> YAHOOOO!");
+        }*/
+
+        if (_meshCollider.bounds.Contains(Input.mousePosition))
+        {
+            Debug.Log("Inside");
+        }
+
+
+     
+            
+
+    }
+
+    private void OnDoubleClickHandler()
+    {
+        Debug.Log("[HexagonRenderer]->OnDoubleClickHandler");
+         /*_screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+
+         Vector2 distance = (InputManager.Instance.mousePosition3D - _screenPoint).normalized;
+         Debug.Log("distance=" + distance);
+
+         float dotProd = Vector3.Dot(transform.right, distance);
+         Debug.Log("dotProd=" + dotProd);
+         float dotProdModulo = Mathf.Abs(dotProd);
+         Debug.Log("dotProdModulo=" + dotProdModulo);*/
+
+         Boolean isInside = ContainsPoint(vertices, InputManager.Instance.mouseScreenPosition);
+         Debug.Log("1 isInside=" + isInside);
+
+        if (_meshCollider.bounds.Contains(InputManager.Instance.mousePosition3D))
+        {
+            Debug.Log("Inside 2");
+        }
+    }
+
+    public Boolean ContainsPoint(Vector3[] polyPoints, Vector3 p ) 
+    { 
+        var j = polyPoints.Length - 1;
+        var inside = false; 
+        for (int i = 0; i < polyPoints.Length; j = i++) 
+        { 
+            if (((polyPoints[i].y <= p.y && p.y<polyPoints[j].y) || (polyPoints[j].y <= p.y && p.y<polyPoints[i].y)) && 
+                (p.x<(polyPoints[j].x - polyPoints[i].x) * (p.y - polyPoints[i].y) / (polyPoints[j].y - polyPoints[i].y) + polyPoints[i].x)) 
+                inside = !inside; 
+        }
+        return inside; 
+    }
+
     public void DrawHexagon()
     {
         Mesh mesh = new Mesh();
 
-        Vector3[] vertices = new Vector3[6];
-        vertices[0] = new Vector3(-_width / 2, Mathf.Sqrt(_width*1.5f));
+       
+        vertices[0] = new Vector3(-_width / 2, Mathf.Sqrt(_width * 1.5f));
         vertices[1] = new Vector3(_width / 2, Mathf.Sqrt(_width * 1.5f));
         vertices[2] = new Vector3(_width, 0);
-        vertices[3] = new Vector3(_width / 2, -Mathf.Sqrt(_width * 1.5f) );
-        vertices[4] = new Vector3(-_width / 2, -Mathf.Sqrt(_width * 1.5f) );
+        vertices[3] = new Vector3(_width / 2, -Mathf.Sqrt(_width * 1.5f));
+        vertices[4] = new Vector3(-_width / 2, -Mathf.Sqrt(_width * 1.5f));
         vertices[5] = new Vector3(-_width, 0);
 
         mesh.vertices = vertices;
 
+
+        Vector2[] uvs = new Vector2[vertices.Length];
+
+        for (int i = 0; i < uvs.Length; i++)
+        {
+            uvs[i] = new Vector2(vertices[i].x, vertices[i].z);
+        }
+        mesh.uv = uvs;
+
+
         mesh.triangles = new int[] { 0, 4, 5, 0, 3, 4, 0, 1, 3, 1, 2, 3 };
         GetComponent<MeshRenderer>().material = _material;
 
+       
+
+        mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
+        mesh.Optimize();
+
         GetComponent<MeshFilter>().mesh = mesh;
+        _meshCollider.sharedMesh = null;
         _meshCollider.sharedMesh = mesh;
+
+        
+    }
+
+    private void OnDisable()
+    {
+        InputManager.OnDoubleClickHandler -= OnDoubleClickHandler;
+    }
+
+    public void OnMouseDown()
+    {
+        if (_meshCollider.sharedMesh.bounds.Contains(InputManager.Instance.mousePosition3D))
+        {
+            Debug.Log("Inside 3");
+        }
     }
 }
--- Assets/ShapeMask2D/Scripts/InputManager.cs
+++ Assets/ShapeMask2D/Scripts/InputManager.cs
@@ -12,6 +12,30 @@ public class InputManager : MonoBehaviour, IPointerClickHandler
     private float timeClickDelay = 1f;
     private float _lastTimeClick;
     public LayerMask whatIsClickable;
+    public Vector2 mouseScreenPosition;    //Where the mouse is on the screen
+
+    [HideInInspector] 
+    public Vector3 mousePosition3D; //Location in 3D space of the mouse cursor	
+    [HideInInspector] 
+    public bool isValid;          //Is the mouse location valid?
+
+    Ray mouseRay;                                   //A ray that will be used to find the mouse
+    RaycastHit hitInfo;                                 //A RaycastHit which will store information about a raycast
+
+    public delegate void ActionDoubleClick();
+    public static event ActionDoubleClick OnDoubleClickHandler;
+
+    public void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(this);
+        }
+    }
 
     public void OnPointerClick(PointerEventData eventData)
     {
@@ -39,17 +63,40 @@ public class InputManager : MonoBehaviour, IPointerClickHandler
     private void DoubleClickHandler()
     {
         //Debug.Log("[InputManager]->DoubleClickHandler");
-        
-        Ray rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hitInfo;
-        if (Physics.Raycast(rayOrigin, out hitInfo, 100, whatIsClickable))
+
+        //Assume the mouse location isn't valid
+        isValid = false;
+
+        //This is platform specific code. Any code that isn't in the appropriate section
+        //is effectively turned into a comment (essentialy doesn't exist when the project is built).
+        //If this is a mobile platform (Android, iOS, or WP8)... 
+        #if UNITY_ANDROID || UNITY_IOS || UNITY_WP8
+		    //...and if it isn't using touch aiming, leave
+			if (!isTouchAiming)
+			    return;
+        #else
+            //...otherwise, record the mouse's position to the screenPosition variable
+            mouseScreenPosition = Input.mousePosition;
+        #endif
+
+        //Create a ray that extends from the main camera, through the mouse's position on the screen
+        //into the scene
+        mouseRay = Camera.main.ScreenPointToRay(mouseScreenPosition);
+
+        if (Physics.Raycast(mouseRay, out hitInfo, 100f, whatIsClickable))
         {
             //TODO: here we can use Interface if we will have lots of different shapes in future
             HexagonRenderer hexagon = hitInfo.collider.GetComponent<HexagonRenderer>();
-
+            
             if (hexagon != null)
             {
                 Debug.Log("[InputManager]->DoubleClickHandler: Hexagon!");
+                //...the mouse position is valid...
+                isValid = true;
+                //...and record the point in 3D space that the ray hit the whatIsClickable
+                mousePosition3D = hitInfo.point;
+
+                OnDoubleClickHandler?.Invoke();
             }
         }
     }
